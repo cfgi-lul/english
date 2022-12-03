374dba7a406fb2d6912131b6075fe23c9c58193b
cfgi <cfgi@yandex.ru>
2022-12-04 00:36:03 +0500

    change from graddle to maven + gitignore for angular app

--- translation-server-app/src/main/java/com/maven/translationserverapp/TranslationServerAppApplication.java
+++ translation-server-app/src/main/java/com/maven/translationserverapp/TranslationServerAppApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.maven.translationserverapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TranslationServerAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TranslationServerAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationServerAppApplication.class, args);
+    }
 
 }
